58ea045f075500d58e0d2f0b643c2d3e257d7844
SnehalPatil1244 <patilsnehal1469@gmail.com>
2025-02-13 14:24:17 +0530

    Array

---
+++ 07 Array/Create Array of 7 elements with null initialization and display it.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int Arr[7]={};
+
+    printf("Element of 0 index : %d\n" ,Arr[0]);
+    printf("Element of 1 index : %d\n" ,Arr[1]);
+    printf("Element of 2 index : %d\n" ,Arr[2]);
+    printf("Element of 3 index : %d\n" ,Arr[3]);
+    printf("Element of 4 index : %d\n" ,Arr[4]);
+    printf("Element of 5 index : %d\n" ,Arr[5]);
+    printf("Element of 6 index : %d\n" ,Arr[6]);
+
+    getch();
+    return 0;
+
+}
---
+++ 07 Array/without initialization & display All Elements.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int No[5];
+
+    printf("========================Elements of the array==================\n");
+
+    printf("Elements: %d\n"  ,No[0]);
+    printf("Elements: %d\n"  ,No[1]);
+    printf("Elements: %d\n"  ,No[2]);
+    printf("Elements: %d\n"  ,No[3]);
+    printf("Elements: %d\n"  ,No[4]);
+
+    getch();
+    return 0;
+}
