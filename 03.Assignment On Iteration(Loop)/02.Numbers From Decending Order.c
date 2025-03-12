3c283170a0ce5cf440864bfd3f06e89b9b0849bc
Snehal Patil <patilsnehal1469@gmail.com>
2025-03-12 22:15:09 +0530

    Looping

---
+++ 03.Assignment On Iteration(Loop)/02.Numbers From Decending Order.c
@@ -0,0 +1,16 @@
+///Write a program that uses a loop to print the numbers from 10 to 1 in descending order.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i =0;
+
+    for(i=10; i>=1; i--)
+    {
+        printf("\n %d",i);
+    }
+    getch();
+    return 0;
+}
